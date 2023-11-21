1a0f34be8446addea55b06e2c468b7170da9f10c
Marcello Leone <marcelloleone975@gmail.com>
2023-11-21 16:45:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,113 @@
+'use strict'
+
+/*******
+ * FUNCTIONS
+ ********/
+
+function creaCella(htmlElement, className, htmlValue) {
+    const element = document.createElement(htmlElement);
+    element.classList.add(className);
+    element.innerText = htmlValue;
+
+    element.addEventListener('click', function () {
+        alert(htmlValue)
+    })
+    return element
+
+}
+
+function myAppendElement(containerElement, htmlElement) {
+    containerElement.append(htmlElement);
+};
+
+function reset(htmlElement) {
+    htmlElement.innerHTML = '';
+};
+
+function creaGriglia(cellNumber) {
+
+    let fragmentGriglia = document.createDocumentFragment();
+
+    for (let i = 1; i <= cellNumber; i++) {
+        //3.1creo la cella
+        const element = document.createElement('div');
+        element.classList.add('cell');
+        element.style.width = `calc(100%/${Math.sqrt(cellNumber)})`;
+        element.style.height = element.style.width;
+
+
+        element.addEventListener('click', function () {
+            console.log('cliccato')
+        })
+        element.innerText = i;
+        fragmentGriglia.append(element);
+    }
+
+    return fragmentGriglia;
+};
+
+
+function campoMinato() {
+
+    //1creo griglia
+    const contenitoreGrilia = document.querySelector('.griglia');
+    reset(contenitoreGrilia);
+
+    //2prendo contenitore griglia
+    //devo collegare il menu a tendina con la rispettiva classe di caselle 
+
+    let cellNumber;
+    //per leggere valore di un campo di input usi VALUE
+    //il case utilizza operatore di identicita per confrontare i valori, due opzioni:
+
+    //1) o tutti input dati dal utente li trasformiamo in numeri
+    let level =Number(document.getElementById('difficolta').value);
+
+    console.log(level)
+
+    switch (level) {
+        //2) o dentro lo switch dovranno esserci dei confronti di tipo stringa: ex-. case '2' non case 2
+        case 2:
+            cellNumber = 81;
+
+            break;
+        case 3:
+            cellNumber = 49;
+            break;
+        case 1:
+        default:
+            cellNumber = 100;
+    }
+
+    console.log(cellNumber)
+
+
+
+    //3definisco num celle iniziali
+    const fragmentGriglia = creaGriglia(cellNumber);
+
+    contenitoreGrilia.append(fragmentGriglia)
+
+
+
+
+    //4per ogni numero di celle creo cella
+    //5appendo cella a contenitore di gioco
+
+
+}
+
+
+/**** 
+MAIN
+****/
+
+
+//identifico il bottone che compierà l'azione andandolo a selezionare tramite il suo ID
+const startButton = document.getElementById('start');
+
+//aggiungo il fatto che al click scatenerà un evento
+startButton.addEventListener('click', campoMinato)
+
+
+
